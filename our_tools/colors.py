1d1667982e9c71f4dad169889cede52a2b9b5c6b
Alberto Narro <alberto-city@hotmail.com>
2023-09-21 15:31:44 -0600

    Ajuste de colores

--- our_tools/colors.py
+++ our_tools/colors.py
@@ -3,32 +3,32 @@ import numpy as np
 colores = {
     "rojo": {
         "nombre": "Rojo",
-        "bajo": np.array([0, 50, 50]),
-        "alto": np.array([10, 255, 255])
+        "bajo": np.array([0, 100, 100]),  # Rango de rojos más pequeño
+        "alto": np.array([5, 255, 255])
     },
     "naranja": {
         "nombre": "Naranja",
-        "bajo": np.array([10, 50, 50]),
-        "alto": np.array([20, 255, 255])
+        "bajo": np.array([10, 100, 100]),  # Rango de naranjas más pequeño
+        "alto": np.array([15, 255, 255])
     },
     "amarillo": {
         "nombre": "Amarillo",
-        "bajo": np.array([20, 50, 50]),
-        "alto": np.array([30, 255, 255])
+        "bajo": np.array([20, 100, 100]),  # Rango de amarillos más pequeño
+        "alto": np.array([25, 255, 255])
     },
     "verde": {
         "nombre": "Verde",
-        "bajo": np.array([30, 50, 50]),
-        "alto": np.array([60, 255, 255])
+        "bajo": np.array([40, 100, 100]),  # Rango de verdes más pequeño
+        "alto": np.array([50, 255, 255])
     },
     "azul": {
         "nombre": "Azul",
-        "bajo": np.array([90, 50, 50]),
-        "alto": np.array([130, 255, 255])
+        "bajo": np.array([100, 100, 100]),  # Rango de azules más pequeño
+        "alto": np.array([110, 255, 255])
     },
     "morado": {
         "nombre": "Morado",
-        "bajo": np.array([120, 50, 50]),
-        "alto": np.array([160, 255, 255])
+        "bajo": np.array([130, 100, 100]),  # Rango de morados más pequeño
+        "alto": np.array([140, 255, 255])
     },
 }
